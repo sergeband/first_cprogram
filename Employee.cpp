616426f3d6d1ef84b769bcbab0c0216d6bab2876
sergeband <andaomejob@gmail.com>
2013-06-22 17:29:04 +0200

    End of first part

--- Employee.cpp
+++ Employee.cpp
@@ -12,4 +12,67 @@ namespace Records {
 		mSalary = kDefaultStartingSalary;
 		fHired = false;
 	}
+	
+	void Employee::promote(int inRaiseAmount){
+		setSalary(getSalary() + inRaiseAmount);
+	}
+	
+	void Employee::demote(int inDemeritAmount){
+		setSalary(getSalary() - inDemeritAmount);
+	}
+	
+	void Employee::hire(){
+		fHired = true;
+	}
+	
+	void Employee::fire(){
+		fHired = false;
+	}
+	
+	void Employee::display(){
+		cout << "Employee:" <<getLastName() <<", "<<getFirstName() <<endl;
+		cout <<"----------------------------- "<<endl;
+		cout <<(fHired ? "Current Employee" : "Former Employee") <<endl;
+		cout << "Employee Number: " <<getEmployeeNumber() << endl;
+		cout << "Salary: $" <<getSalary() <<endl;
+		cout <<endl;
+	}
+	
+	void Employee::setFirstName(string inFirstName){
+		mFirstName = inFirstName;
+	}
+	
+	string Employee::getFirstName(){
+		return mFirstName; 
+	}
+	
+	void Employee::setLastName(string inLastName){
+		mLastName = inLastName;
+	}
+	
+	string Employee::getLastName(){
+		return mLastName; 
+	}
+	
+	void Employee::setEmployeeNumber(int inEmployeeNumber){
+		mEmployeeNumber = inEmployeeNumber;
+	}
+	
+	int Employee::getEmployeeNumber(){
+		return mEmployeeNumber;
+	}
+	
+	void Employee::setSalary(int inSalary){
+		mSalary = inSalary;
+	}
+	
+	int Employee::getSalary(){
+		return mSalary;
+	}
+	
+	bool Employee::getIsHired(){
+		return fHired;
+	}
+	
+	
 }
--- EmployeeTest.cpp
+++ EmployeeTest.cpp
@@ -5,7 +5,18 @@
 using namespace std;
 using namespace Records;
 
-int main(){
+int main(int argc, char** argv){
+	cout <<"Testing the Employee class." <<endl;
+	Employee emp;
+	
+	emp.setFirstName("Marni");
+	emp.setLastName("Kleper");
+	emp.setEmployeeNumber(71);
+	emp.setSalary(50000);
+	emp.promote();
+	emp.promote(50);
+	emp.hire();
+	emp.display();
 	
 	return 0; 
 	
